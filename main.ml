fef402a2b964c4b7c8c18a289abeb36a1ac4d69d
Michael Whittaker <mjw297@cornell.edu>
2014-06-14 05:51:15 +0000

    Interpreter now reads from file.
    I figure this will make testing a bit easier.

--- main.ml
+++ main.ml
@@ -1,6 +1,12 @@
 let main () =
+  if Array.length Sys.argv < 2 then (
+    print_endline "usage: arith <filename>";
+    exit 0
+  ) else ();
+  
+  let filename = Sys.argv.(1) in 
   try
-    let lexbuf = Lexing.from_channel stdin in
+    let lexbuf = Lexing.from_channel (open_in filename) in
     Parser.input Lexer.token lexbuf
   with 
     | End_of_file -> exit 0
