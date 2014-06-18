c644c21c69d005889dc72a8b4d8999d2a6d66419
Michael Whittaker <mjw297@cornell.edu>
2014-06-18 05:13:54 +0000

    Arith can now take multiple files

--- arith.ml
+++ arith.ml
@@ -1,12 +1,14 @@
-let main () =
-  if Array.length Sys.argv < 2 then (
-    print_endline "usage: arith <filename>";
-    exit 0
-  ) else ();
-  
-  let filename = Sys.argv.(1) in 
-  let lexbuf = Lexing.from_channel (open_in filename) in
+let eval_file (filename: string) : unit = 
+  let lexbuf  = Lexing.from_channel (open_in filename) in
   let program = Parser.input Lexer.token lexbuf in
-  List.iter (fun f -> print_float f; print_endline "") (Eval.eval program)
+  List.iter (Printf.printf "%f\n") (Eval.eval program)
+
+let main () =
+  match Array.to_list Sys.argv with
+    | [] | [_] -> begin
+      print_endline "usage: arith <filename>";
+      exit 0
+    end
+    | _::filenames -> List.iter eval_file filenames
 
 let _ = Printexc.print main ()
